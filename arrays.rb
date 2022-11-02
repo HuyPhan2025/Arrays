ac6632e43ca97b6459ef7d9e9dd4f34584a6bd39
Huy Phan <huyphan520@gmail.com>
2022-11-02 17:45:01 -0500

    Explain array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ names = ["erik","jo","lin","mike"]
 house_numbers = [10,17,8,20]
 temps = [35.5,25.77.9,50.6]
 moods = [happy,sad,angry,excited]
+
+# The names "ivy" and "isabel" will be push to the front before "erik".
+names.push("ivy","isabel")
+
+# All numbers will be shift one space forward. The 10 will be gone.
+house_numbers.shift
+
+# Add the 50,5 to the array at the front.
+temps.unshift(50.5)
+
+# Tell you the number of elements in array.
+[happy,sad,angry,excited].length
